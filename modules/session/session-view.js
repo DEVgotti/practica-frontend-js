fd16aa4360d2ffc1d52ca29a2a1cb63fb1b2f15a
DEVgotti <david.lmonras@gmail.com>
2024-03-21 16:01:20 +0000

    Setup session view

---
+++ modules/session/session-view.js
@@ -0,0 +1,13 @@
+export const buildAnonymousSession = () => {
+  return `<ul>
+    <li>Login</li>
+    <li>Signup</li>
+  </ul>`
+}
+
+export const buildAuthenticatedSession = () => {
+  return `<ul>
+    <li>Crear anuncio</li>
+    <li>Cerrar sesión</li>
+  </ul>`
+}
